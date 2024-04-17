59c63008c42b9defca60f78738bd3ce7cd64c553
nabilmuafa <nmuafa@gmail.com>
2024-04-17 09:02:15 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroom/auth/HoomgroomBeAuthApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.hoomgroom.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoomgroomBeAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoomgroomBeAuthApplication.class, args);
+    }
+
+}

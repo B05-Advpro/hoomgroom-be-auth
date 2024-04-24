b54881a41b3cdffb8e8cee2603c57b0bd82f43f9
nabilmuafa <nmuafa@gmail.com>
2024-04-24 19:34:17 +0700

    [RED] Add tests for User model (birthDate, sex, role)

--- src/test/java/id/ac/ui/cs/advprog/hoomgroom/auth/model/UserTest.java
+++ src/test/java/id/ac/ui/cs/advprog/hoomgroom/auth/model/UserTest.java
@@ -3,6 +3,9 @@ package id.ac.ui.cs.advprog.hoomgroom.auth.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 import static org.junit.jupiter.api.Assertions.*;
 class UserTest {
     UserBuilder userBuilder;
@@ -56,5 +59,43 @@ class UserTest {
     @Test
     void testCreateUserInvalidEmail() {
         assertThrows(IllegalArgumentException.class, () -> userBuilder.email("Gak Punya Email 😭").build());
+    }
+
+    @Test
+    void testCreateUserValidDate() {
+        User user = userBuilder.birthDate("2004-01-01").build();
+        assertEquals("2004-01-01", user.getBirthDate().format(DateTimeFormatter.ISO_LOCAL_DATE));
+    }
+
+    @Test
+    void testCreateUserInvalidDate() {
+        assertThrows(DateTimeParseException.class, () -> userBuilder.birthDate("37826957812").build());
+    }
+
+    @Test
+    void testCreateUserValidSex() {
+        User userM = userBuilder.sex('M').build();
+        User userF = userBuilder.sex('F').build();
+        assertEquals('M', userM.getSex());
+        assertEquals('F', userF.getSex());
+    }
+
+    @Test
+    void testCreateUserInvalidSex() {
+        assertThrows(IllegalArgumentException.class, () -> userBuilder.sex('N').build());
+    }
+
+    @Test
+    void testCreateUserValidRoles() {
+        User user1 = userBuilder.role("USER").build();
+        User user2 = userBuilder.role("ADMIN").build();
+
+        assertEquals("USER", user1.getRole());
+        assertEquals("ADMIN", user2.getRole());
+    }
+
+    @Test
+    void testCreateUserNonexistentRole() {
+        assertThrows(IllegalArgumentException.class, () -> userBuilder.role("HACKER").build());
     }
 }

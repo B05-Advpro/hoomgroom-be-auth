e2ca0802fbb49030736f551198d11ae886a9dea4
nabilmuafa <nmuafa@gmail.com>
2024-04-24 18:31:30 +0700

    [RED] Create & modify test for User model using Builder Pattern

--- src/test/java/id/ac/ui/cs/advprog/hoomgroom/auth/model/UserTest.java
+++ src/test/java/id/ac/ui/cs/advprog/hoomgroom/auth/model/UserTest.java
@@ -5,16 +5,56 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 class UserTest {
-    User valid_user;
+    UserBuilder userBuilder;
     @BeforeEach
     void setUp() {
-        this.valid_user = new User();
-        this.valid_user.setUsername("cbkadal");
-        this.valid_user.setPassword("{bcrypt}$2a$12$1kU3dPF2KhSWpLyG7CrWhObGaKiEjdhZ9H3M.TdIimKyaiRM9N80O");
+        this.userBuilder = new User.builder();
+    }
+    @Test
+    void testCreateUserValidUsername() {
+        User user = userBuilder.username("cbkadal").build();
+        assertEquals("cbkadal", user.getUsername());
     }
 
     @Test
-    void testCreateUserValidUsername(){
-        assertEquals("cbkadal", this.valid_user.getUsername());
+    void testCreateUserInvalidUsername() {
+        assertThrows(IllegalArgumentException.class, userBuilder.username("").build());
+        assertThrows(IllegalArgumentException.class, userBuilder.username("~@#$!@%@#$%!$^!#$%").build());
+    }
+
+    @Test
+    void testCreateUserValidPassword() {
+        User user = userBuilder.password("adprosukses123").build();
+        assertEquals("{bcrypt}$2a$12$qwDzccZRqXQ6Dm3auq5Sx.nXegoc0vfuO84DoLVLE.sTgYGjM/kw2",
+                user.getPassword());
+    }
+
+    @Test
+    void testCreateUserInvalidPassword() {
+        assertThrows(IllegalArgumentException.class, userBuilder.password("").build());
+    }
+
+    @Test
+    void testCreateUserFullName() {
+        User user = userBuilder.fullName("Baba Fitt").build();
+        assertEquals("Baba Fitt",
+                user.getFullName());
+    }
+
+    @Test
+    void testCreateUserDefaultFullName() {
+        User user = userBuilder.fullName("").build();
+        assertEquals("Pengguna", user.getFullName());
+    }
+
+    @Test
+    void testCreateUserValidEmail() {
+        User user = userBuilder.email("baba@gmail.com").build();
+        assertEquals("baba@gmail.com", user.getEmail());
+    }
+
+    @Test
+    void testCreateUserInvalidEmail() {
+        assertThrows(userBuilder.email("Gak Punya Email 😭").build());
     }
 }

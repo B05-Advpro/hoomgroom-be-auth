a34dae1d89450f6dccf4cc894300519734fc5427
nabilmuafa <nmuafa@gmail.com>
2024-04-24 19:19:29 +0700

    [GREEN] Implement User and UserBuilder (username, fullName, password, email)

--- src/main/java/id/ac/ui/cs/advprog/hoomgroom/auth/model/User.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroom/auth/model/User.java
@@ -5,35 +5,72 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "users")
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     private String username;
 
+    @Setter
     private String fullName;
 
+    @Setter
     private String password;
 
+    @Setter
     private String email;
 
+    @Setter
     private LocalDate birthDate;
 
+    @Setter
     private char sex;
 
+    @Setter
     private String role;
 
     public static UserBuilder builder() {
         return new UserBuilder();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
--- src/main/java/id/ac/ui/cs/advprog/hoomgroom/auth/model/UserBuilder.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroom/auth/model/UserBuilder.java
@@ -1,27 +1,42 @@
 package id.ac.ui.cs.advprog.hoomgroom.auth.model;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.regex.Pattern;
+
 public class UserBuilder {
+    private final Pattern emailPattern = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+    private final Pattern usernamePattern = Pattern.compile("^[a-z0-9._]+$");
     private String username;
     private String fullName;
     private String password;
     private String email;
 
     public UserBuilder username(String username) {
+        if (username.isEmpty() || !usernamePattern.matcher(username).matches()) {
+            throw new IllegalArgumentException();
+        }
         this.username = username;
         return this;
     }
 
     public UserBuilder fullName(String fullName) {
-        this.fullName = fullName;
+        this.fullName = fullName.isEmpty() ? "Pengguna" : fullName;
         return this;
     }
 
-    public UserBuilder password(String username) {
+    public UserBuilder password(String password) {
+        if (password.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
         this.password = password;
         return this;
     }
 
     public UserBuilder email(String email) {
+        if (!emailPattern.matcher(email).matches()) {
+            throw new IllegalArgumentException();
+        }
         this.email = email;
         return this;
     }
@@ -31,7 +46,7 @@ public class UserBuilder {
         user.setUsername(this.username);
         user.setFullName(this.fullName);
         user.setPassword(this.password);
-        user.setEmail(this.password);
+        user.setEmail(this.email);
         return user;
     }
 }

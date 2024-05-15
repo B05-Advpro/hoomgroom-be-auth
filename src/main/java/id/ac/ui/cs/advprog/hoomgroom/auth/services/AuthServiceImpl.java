7df46cd581d403e391b964c14cb944a5a42c0e1e
nabilmuafa <nmuafa@gmail.com>
2024-05-15 15:49:11 +0700

    [REFACTOR] Add set auth to securitycontext

--- src/main/java/id/ac/ui/cs/advprog/hoomgroom/auth/services/AuthServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroom/auth/services/AuthServiceImpl.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -56,10 +58,11 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 request.getUsername(),
                 request.getPassword()
         ));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
         User user = userRepository.findByUsername(request.getUsername()).orElseThrow();
         Map<String, Object> extraClaims = new HashMap<>();
         extraClaims.put("role", user.getRole());
